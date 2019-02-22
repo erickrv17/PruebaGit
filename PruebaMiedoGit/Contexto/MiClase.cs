30c05affa59b221205330768520b2528e986f7fd
Erick Rodriguez Vega <erickrvrv1717@gmail.com>
2019-02-21 23:19:08 -0600

    MiClase

---
+++ PruebaMiedoGit/Contexto/MiClase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contexto
+{
+    public class MiClase
+    {
+        public int idProducto { get; set; }
+
+        public int numeroCelular { get; set; }
+    }
+}
