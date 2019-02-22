8965822cc422fdcf8b424d349119914ee6c84799
Erick Rodriguez Vega <erickrvrv1717@gmail.com>
2019-02-21 23:29:36 -0600

    Actualizacion

---
+++ PruebaMiedoGit/Contexto/Clase2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contexto
+{
+    public class Clase2
+    {
+        public int MyProperty { get; set; }
+    }
+}
